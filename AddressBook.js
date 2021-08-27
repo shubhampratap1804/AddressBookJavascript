c9b3e0cf04ea2ec4b1630d95fba84f2b24a74d98
shubhampratap1804 <samsunggalaxygrand2pratap@gmail.com>
2021-08-27 18:16:16 +0530

    Declared: attributes and created constructor

--- AddressBook.js
+++ AddressBook.js
@@ -1,1 +1,23 @@
 console.log("Welcome to address book using javascript");
+
+class AddressBook{
+    firstName;
+    lastName;
+    address;
+    city;
+    state;
+    zip;
+    phoneNumber;
+    email;
+    
+    constructor(firstName, lastName, address, city, state, zip, phoneNumber, email) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.address = address;
+    this.city = city;
+    this.state = state;
+    this.zip = zip;
+    this.phoneNumber = phoneNumber;
+    this.email = email;
+    }
+}
